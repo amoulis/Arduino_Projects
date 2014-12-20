5da092cdad3d97e84be5763fd11179e3d7464ac6
amoulis <adrien.moulis@gmail.com>
2014-12-20 11:56:53 +0100

    start of detection algorithm

--- people_room_monitoring/people_room_monitoring_soft/people_room_monitoring_soft.ino
+++ people_room_monitoring/people_room_monitoring_soft/people_room_monitoring_soft.ino
@@ -31,19 +31,57 @@ void setup()
 void loop()
 {
   int in = 0, inside = 0, out = 0;
-  int S1_act = 0, S2_act = 0;
   t_sensor sensor1, sensor2;
   
   sensor1.led = PIN_LED_1;
   sensor1.sensor = PIN_SENSOR_1;
+  sensor1.activated = 0;
   
   sensor2.led = PIN_LED_2;
   sensor2.sensor = PIN_SENSOR_2;
+  sensor2.activated = 0;
   // Light up LED on detection
   light_up_led(sensor1);
   light_up_led(sensor2);
   
   // Algo
+  
+  if(isDetected(sensor1.sensor))
+  {
+    if(sensor2.activated == 0)
+      sensor1.activated = 1;
+    else if(sensor2.activated == 1)
+    {
+      sensor2.activated = 0;
+      inside--;
+      out++;
+    }  
+  }
+  else if (isDetected(sensor2.sensor))
+  {
+    if(sensor1.activated == 0)
+      sensor1.activated = 1;
+    else if(sensor1.activated == 1)
+    {
+      sensor1.activated = 0;
+      inside++;
+      in++;
+    }  
+  
+  }
+  
+  if(inside < 0)
+    inside = 0;
+    
+  Serial.println("\n People Inside: ");
+  Serial.println(inside);
+  Serial.println("\n nb of enters: ");
+  Serial.println(in);
+  Serial.println("\n nb left: ");
+  Serial.println(out);
+  
+  delay(1000);
+  
 }
 
 
